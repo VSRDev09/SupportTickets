7b962d2cfe4cead800813a9cad90da33376f586b
VSR <vinissantosz0009@gmail.com>
2026-05-03 15:54:08 -0300

    feat: adicionando Enums

---
+++ src/Domain/Enums/PerfilUsuario.cs
@@ -0,0 +1,8 @@
+namespace SupportTickets.Domain.Enums;
+
+public enum PerfilUsuario
+{
+    Admin = 1,
+    Atendente = 2,
+    Usuario = 3
+}
---
+++ src/Domain/Enums/StatusChamado.cs
@@ -0,0 +1,9 @@
+namespace SupportTickets.Domain.Enums;
+
+public enum StatusChamado
+{
+    Aberto = 1,
+    Executando = 2,
+    Finalizado = 3,
+    Cancelado = 4
+}
